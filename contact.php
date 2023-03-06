140f4775fd00d4bcf574ffd81bb823b9e32c569b
Micael <zacio007@gmail.com>
2023-03-06 10:28:43 +0100

    reprise html

---
+++ contact.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form action="" method="post">
+            <label for="nom">Nom</label>
+            <input type="text" name="nom" id="nom">
+            <label for="mail">Mail</label>
+            <input type="mail" name="mail" id="mail">
+            <label for="contactText">Message</label>
+            <textarea name="contactText" id="contactText" cols="30" rows="10"></textarea>
+        </form>
+    </div>
+    <div>
+
+    </div>
+</body>
+</html>
